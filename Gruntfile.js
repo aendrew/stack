614835c7ee62a19fd799f37866aee5f44be77cba
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2014-11-07 17:42:20 +0000

    Adding gruntfile, swipe support.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['bower_components/d3/d3.js', 'stack.js'],
+        // the location of the resulting JS file
+        dest: '.tmp/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
--- stack.js
+++ stack.js
@@ -8,7 +8,9 @@ function stack(){
       windowHeight,
       dispatch = d3.dispatch("scroll", "activate", "deactivate"),
       touchy = "ontouchstart" in document,
-      resize = touchy ? resizeTouchy : resizeNoTouchy,
+      touchStartVal,
+      touchEndVal,
+      resize = resizeNoTouchy,
       i = NaN,
       y = 0,
       yt,
@@ -23,61 +25,88 @@ function stack(){
 
   var body = d3.select("body")
       .style("margin", 0)
-      .style("padding", 0)
-      .style("background", "#333");
+      .style("padding", 0);
+      // .style("background", "#333");
 
-  if (touchy) {
-    section
-        .style("position", "relative");
+  var background = d3.select("body").insert("div", "section")
+      .style("background", "#000")
+      .style("box-shadow", "0 8px 16px rgba(0,0,0,.3)")
+      .style("padding", "1px 0")
+      .style("margin-top", "-1px")
+      .style("z-index", 0);
 
-    d3.select(window)
-        .on("resize.stack", resize)
-        .each(resize);
-  } else {
-    var background = d3.select("body").insert("div", "section")
-        .style("background", "#000")
-        .style("box-shadow", "0 8px 16px rgba(0,0,0,.3)")
-        .style("padding", "1px 0")
-        .style("margin-top", "-1px")
-        .style("z-index", 0);
+  section
+      .style("display", "none")
+      .style("opacity", 0)
+      .style("z-index", 0);
 
-    section
-        .style("display", "none")
-        .style("opacity", 0)
-        .style("z-index", 0);
+  var sectionAndBackground = d3.selectAll(section[0].concat(background.node()))
+      .style("position", "fixed")
+      .style("left", 0)
+      .style("top", 0)
+      .style("width", "100%");
 
-    var sectionAndBackground = d3.selectAll(section[0].concat(background.node()))
-        .style("position", "fixed")
-        .style("left", 0)
-        .style("top", 0)
-        .style("width", "100%");
+  var indicator = d3.select("body").append("div")
+      .attr("class", "indicator")
+    .selectAll("div")
+      .data(d3.range(section.size()))
+    .enter().append("div")
+      .style("position", "absolute")
+      .style("z-index", 10)
+      .style("left", 0)
+      .style("width", "3px")
+      .style("background", "linear-gradient(to top,black,white)");
 
-    var indicator = d3.select("body").append("div")
-        .attr("class", "indicator")
-      .selectAll("div")
-        .data(d3.range(section.size()))
-      .enter().append("div")
-        .style("position", "absolute")
-        .style("z-index", 10)
-        .style("left", 0)
-        .style("width", "3px")
-        .style("background", "linear-gradient(to top,black,white)");
+  var sectionPrevious = d3.select(null),
+      sectionCurrent = d3.select(section[0][0]),
+      sectionNext = d3.select(section[0][1]);
 
-    var sectionPrevious = d3.select(null),
-        sectionCurrent = d3.select(section[0][0]),
-        sectionNext = d3.select(section[0][1]);
+  var touchyStart = function() {
+    touchStartVal = d3.event.changedTouches[0].clientY;
+  };
 
-    d3.select(window)
-        .on("resize.stack", resize)
-        .on("scroll.stack", reposition)
-        .on("keydown.stack", keydown)
-        .each(resize);
+  var touchyEnd = function() {
+    var delta;
+    touchEndVal = d3.event.changedTouches[0].clientY;
+    if (touchStartVal > touchEndVal) {
+      delta = 1;
+    } else if (touchStartVal < touchEndVal) {
+      delta = -1;
+    }
 
-    d3.timer(function() {
-      reposition();
-      return true;
-    });
-  }
+    var y0 = isNaN(yt) ? y : yt;
+
+    yt = Math.max(0, Math.min(n - 1, (delta > 0
+        ? Math.floor(y0 + (1 + scrollRatio) / 2)
+        : Math.ceil(y0 - (1 - scrollRatio) / 2)) + delta));
+
+    d3.select(document.documentElement)
+        .interrupt()
+      .transition()
+        .duration(500)
+        .tween("scroll", function() {
+          var i = d3.interpolateNumber(pageYOffset, yt * windowHeight);
+          return function(t) { scrollTo(0, i(t)); };
+        })
+        .each("end", function() { yt = NaN; });
+
+    d3.event.preventDefault();
+  };
+
+  d3.select(window)
+      .on("resize.stack", resize)
+      .on("scroll.stack", reposition)
+      .on("keydown.stack", keydown)
+      .on("touchstart", touchyStart)
+      // .on("touchmove", touchy)
+      .on("touchend", touchyEnd)
+      .each(resize);
+
+
+  d3.timer(function() {
+    reposition();
+    return true;
+  });
 
   function dispatchEvent(event, i) {
     var target = section[0][i], sourceEvent = event.sourceEvent = d3.event;
@@ -105,22 +134,6 @@ function stack(){
       this.__stack__.active = false;
       dispatchEvent({type: "deactivate"}, this.__stack__.index);
     }
-  }
-
-  function resizeTouchy() {
-    var marginBottom = 20;
-
-    sectionHeight = size[1] / size[0] * innerWidth;
-    windowHeight = innerHeight;
-
-    section
-        .style("height", sectionHeight + "px")
-        .style("box-shadow", "0 4px 4px rgba(0,0,0,.3)")
-      .filter(function(d, i) { return i < n - 1; })
-        .style("margin-bottom", marginBottom + "px");
-
-    body
-        .style("font-size", innerWidth / size[0] * fontSize + "px");
   }
 
   function resizeNoTouchy() {
